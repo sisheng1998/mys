100fa76a800fdadd8759a4152ca88772a6b02d83
Sheng <sisheng1998@gmail.com>
2025-03-23 17:40:56 +0800

    style(general): updating styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
 import type { Config } from "tailwindcss"
 import * as tailwindAnimate from "tailwindcss-animate"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config: Config = {
   darkMode: ["class"],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
