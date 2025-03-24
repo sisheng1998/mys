2155ee813797e69489a9e258c63bb95b93e0a68b
Sheng <sisheng1998@gmail.com>
2025-03-24 12:04:05 +0800

    update env

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,10 @@ import { z } from "zod"
 export const env = createEnv({
   server: {
     CONVEX_DEPLOYMENT: z.string().min(1),
+    CONVEX_SITE_URL: z.string().url(),
+    SITE_URL: z.string().url(),
+    JWKS: z.string().min(1),
+    JWT_PRIVATE_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_CONVEX_URL: z.string().url(),
@@ -11,5 +15,9 @@ export const env = createEnv({
   runtimeEnv: {
     CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+    CONVEX_SITE_URL: process.env.CONVEX_SITE_URL,
+    SITE_URL: process.env.SITE_URL,
+    JWKS: process.env.JWKS,
+    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
   },
 })
