d327b048c48651f6a0fb07313f7a18f4e87e30bc
Sheng <sisheng1998@gmail.com>
2025-03-30 16:59:13 +0800

    remove env for local development

--- src/env.ts
+++ src/env.ts
@@ -3,14 +3,12 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
-    CONVEX_DEPLOYMENT: z.string().min(1),
     CONVEX_SITE_URL: z.string().url(),
   },
   client: {
     NEXT_PUBLIC_CONVEX_URL: z.string().url(),
   },
   runtimeEnv: {
-    CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
     CONVEX_SITE_URL: process.env.CONVEX_SITE_URL,
   },
