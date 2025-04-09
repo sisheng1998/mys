b66cfe1965d86ef14f91f33a1aabd945f04b3c13
Sheng <sisheng1998@gmail.com>
2025-04-09 08:17:27 +0800

    style(footer): update footer styling

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { Heart } from "lucide-react"
 
 const Footer = () => (
-  <header className="text-muted-foreground m-2 flex shrink-0 flex-wrap items-center justify-between gap-2 pb-1 text-xs">
+  <header className="text-muted-foreground m-2 flex shrink-0 flex-wrap items-center justify-between gap-2 px-2 pb-1 text-xs">
     <p>
       Made with
       <Heart className="mx-1 mb-1 inline size-3.5" />
