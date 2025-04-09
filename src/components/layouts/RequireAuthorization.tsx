cb8e27c1282425000e2799f753a12468057ccb47
Sheng <sisheng1998@gmail.com>
2025-04-09 08:52:24 +0800

    style(required authorization): update styling

--- src/components/layouts/RequireAuthorization.tsx
+++ src/components/layouts/RequireAuthorization.tsx
@@ -2,12 +2,12 @@
 
 import React, { useState } from "react"
 import { useAuthActions } from "@convex-dev/auth/react"
-import { TriangleAlert } from "lucide-react"
+import { LogOut, TriangleAlert } from "lucide-react"
 import { toast } from "sonner"
 
 import { useRouter } from "@/hooks/use-router"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Button } from "@/components/ui/button"
+import { LoaderButton } from "@/components/ui/loader-button"
 import { useAuth } from "@/contexts/auth"
 
 const RequireAuthorization = ({ children }: { children: React.ReactNode }) => {
@@ -36,27 +36,28 @@ const Unauthorized = () => {
 
   return (
     <main className="flex flex-1 flex-col items-center justify-center p-4">
-      <Alert className="w-full max-w-sm gap-y-2">
+      <Alert className="w-full max-w-sm gap-y-2 has-[>svg]:grid-cols-[calc(var(--spacing)*5)_1fr] [&>svg]:size-5">
         <TriangleAlert />
 
-        <AlertTitle>Unauthorized Access</AlertTitle>
+        <AlertTitle className="text-base">Unauthorized Access</AlertTitle>
 
-        <AlertDescription className="gap-2 text-xs">
-          <p>{`It looks like you don't have access to this application.`}</p>
+        <AlertDescription className="gap-2">
+          <p>{`You don't have access to this application.`}</p>
 
-          <p>
+          <p className="text-xs">
             Try signing in with a different account, or contact admin if you
             think this is a mistake.
           </p>
 
-          <Button
+          <LoaderButton
             size="sm"
-            className="mt-1"
+            className="mt-1.5"
             onClick={handleClick}
-            disabled={isLoading}
+            isLoading={isLoading}
+            icon={LogOut}
           >
-            Okay
-          </Button>
+            Sign Out
+          </LoaderButton>
         </AlertDescription>
       </Alert>
     </main>
